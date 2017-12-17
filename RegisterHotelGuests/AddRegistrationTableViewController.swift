5a919b62717cc0901ec4f501b9147526dfa31413
Amaan Khan <amaancan@gmail.com>
2017-12-16 19:14:46 -0500

    Pickers hide and unhide responding to user interaction.

--- RegisterHotelGuests/AddRegistrationTableViewController.swift
+++ RegisterHotelGuests/AddRegistrationTableViewController.swift
@@ -9,21 +9,117 @@
 import UIKit
 
 class AddRegistrationTableViewController: UITableViewController {
+    
     @IBOutlet weak var firstNameTextField: UITextField!
-    
     @IBOutlet weak var lastNameTextField: UITextField!
-    
     @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var checkInDateLabel: UILabel!
+    @IBOutlet weak var checkInDatePicker: UIDatePicker!
+    @IBOutlet weak var checkOutDateLabel: UILabel!
+    @IBOutlet weak var checkOutDatePicker: UIDatePicker!
     
     //MARK: - QUESTION - "??" ???
     @IBAction func doneBarButtonTapped(_ sender: UIBarButtonItem) {
         let firstName = firstNameTextField.text ?? ""
         let lastName = lastNameTextField.text ?? ""
         let email = emailTextField.text ?? ""
-        
+        let checkInDate = checkInDatePicker.date
+        let checkOutDate = checkOutDatePicker.date
+    
         print("DONE TAPPED")
         print("firstName: \(firstName)")
         print("lastName: \(lastName)")
         print("email: \(email)")
+        print("checkIn: \(checkInDate)")
+        print("checkOut: \(checkOutDate)")
+    }
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        updateDateViews()
+    }
+    
+    let checkInDatePickerCellIndexPath = IndexPath(row: 1, section: 1)
+    let checkOutDatePickerCellIndexPath = IndexPath(row: 3, section: 1)
+
+    var isCheckInDatePickerShown: Bool = false {
+        //Var stores answer to question: Should I show the date picker? Based on response to user touch.
+        didSet {
+            checkInDatePicker.isHidden = !isCheckInDatePickerShown
+        }
+    }
+    var isCheckOutDatePickerShown: Bool = false {
+        didSet {
+            checkOutDatePicker.isHidden = !isCheckOutDatePickerShown
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resetCheckInDatePickerToToday()
+        updateDateViews()
+    }
+
+    func resetCheckInDatePickerToToday() {
+        let midnightToday = Calendar.current.startOfDay(for: Date())
+        checkInDatePicker.minimumDate = midnightToday
+        checkInDatePicker.date = midnightToday
+    }
+    
+    func updateDateViews() {
+        checkOutDatePicker.minimumDate = checkInDatePicker.date.addingTimeInterval(86400)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle  = .medium
+        
+        checkInDateLabel.text = dateFormatter.string(from: checkInDatePicker.date)
+        checkOutDateLabel.text = dateFormatter.string(from: checkOutDatePicker.date)
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            //Sets the height of each cell in tbl view according to need
+        switch (indexPath.section, indexPath.row) {
+        case (checkInDatePickerCellIndexPath.section, checkInDatePickerCellIndexPath.row):
+            return isCheckInDatePickerShown ? 216.0 : 0.0
+        case (checkOutDatePickerCellIndexPath.section, checkOutDatePickerCellIndexPath.row):
+            return isCheckOutDatePickerShown ? 216.0 : 0.0
+        default: return 44.0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+            //When the user taps a cell, deselect the cell—that is, remove its gray highlight, because it doesn't make sense for the cell to remain highlighted after it's been tapped.
+        
+        switch (indexPath.section, indexPath.row) {
+        case (checkInDatePickerCellIndexPath.section, checkInDatePickerCellIndexPath.row - 1):
+            if isCheckInDatePickerShown {
+                isCheckInDatePickerShown = false //unshow selected picker which is already showing
+            } else if isCheckOutDatePickerShown {
+                isCheckOutDatePickerShown = false
+                isCheckInDatePickerShown = true
+            } else {
+                isCheckInDatePickerShown = true
+            }
+            
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            
+        case (checkOutDatePickerCellIndexPath.section, checkOutDatePickerCellIndexPath.row - 1):
+            if isCheckOutDatePickerShown {
+                isCheckOutDatePickerShown = false
+            } else if isCheckInDatePickerShown {
+                isCheckInDatePickerShown = false
+                isCheckOutDatePickerShown = true
+            } else {
+                isCheckOutDatePickerShown = true
+            }
+
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        
+        default: break
+        }
+    }
+    
+    func hideUnhidePickers (indexPath: IndexPath) {
+        
     }
 }
