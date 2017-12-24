0db6c35816dcf74bb922d2e697f8b83af1163062
Amaan Khan <amaancan@gmail.com>
2017-12-24 18:54:50 -0500

    Add: computer var ‘registration’ which makes a Registration obj, RegistrationTblVC without the data source, modal segue from Add button.

--- RegisterHotelGuests/AddRegistrationTableViewController.swift
+++ RegisterHotelGuests/AddRegistrationTableViewController.swift
@@ -73,6 +73,30 @@ class AddRegistrationTableViewController: UITableViewController, SelectRoomTypeT
     }
     var roomType: RoomType? // Add a property to hold the selected room type
     
+    var registration: Registration? {
+        
+        guard let roomType = roomType else { return nil }
+        
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        let email = emailTextField.text ?? ""
+        let checkInDate = checkInDatePicker.date
+        let checkOutDate = checkOutDatePicker.date
+        let numberOfAdults = Int(numberOfAdultsStepper.value)
+        let numberOfChildren = Int(numberOfChildrenStepper.value)
+        let hasWifi = wifiSwitch.isOn
+        
+        return Registration(firstName: firstName,
+                            lastName: lastName,
+                            emailAddress: email,
+                            checkInDate: checkInDate,
+                            checkOutDate: checkOutDate,
+                            numberOfAdults: numberOfAdults,
+                            numberOfChildren: numberOfChildren,
+                            roomType: roomType,
+                            wifi: hasWifi)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         resetCheckInDatePickerToToday()
---
+++ RegisterHotelGuests/RegistrationTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationTableViewController.swift
+//  RegisterHotelGuests
+//
+//  Created by Amaan on 2017-12-24.
+//  Copyright © 2017 Amaan Khan. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationTableViewController: UITableViewController {
+
+    var registrations: [Registration] = [] //Add a property that holds an array of Registration objects
+    
+    // MARK: - Table view data source
+
+}
